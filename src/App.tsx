d80c1292cb6079f77a67e526d0dce502a4c5e508
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2022-05-03 19:44:37 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 import React, { Suspense } from "react";
 import { BrowserView, MobileView } from "react-device-detect";
-import {
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "./assets/styles/main.scss";
+import AvatarGenerator from "./components/AvatarGenerator";
 import { GPRoute } from "./components/GlobalProvider";
 // Importing Pages
 import Template from "./pages";
+import { ThemeProvider, createTheme } from "@mui/material";
 
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#9c27b0",
+    },
+  },
+});
 
 export default function App() {
   return (
     <Suspense fallback="loading...">
       <BrowserView>
-        <Router>
-          <Switch>
-
-            <GPRoute path="/:id" exact component={Template} />
-            <GPRoute path="/" exact component={Template} />
-
-          </Switch>
-        </Router>
+        <ThemeProvider theme={theme}>
+          <div className="main-wrap">
+            <Router>
+              <Switch>
+                <GPRoute path="/" exact component={AvatarGenerator} />
+                <GPRoute path="/template/:id" exact component={Template} />
+                <GPRoute path="/template" exact component={Template} />
+              </Switch>
+            </Router>
+          </div>
+        </ThemeProvider>
       </BrowserView>
       <MobileView>
         <div className="abs top left smartphone">
